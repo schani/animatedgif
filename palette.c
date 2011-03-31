80aa1d1097d1c6e82ed140ce294813cf83e3e47c
Mark Probst <mark.probst@gmail.com>
2011-03-31 23:23:34 +0200

    Octree reduction.

--- palette.c
+++ palette.c
@@ -6,6 +6,8 @@
 
 #include "rwimg/readimage.h"
 
+#include "palette.h"
+
 typedef struct octree octree_t;
 
 struct octree {
@@ -108,18 +110,108 @@ gather_collapse_candidates (octree_t *tree, int *num, octree_t **arr)
 	return is_leaf;
 }
 
+static int
+compare_candidates (const void *p1, const void *p2)
+{
+	octree_t *t1 = *(octree_t**)p1;
+	octree_t *t2 = *(octree_t**)p2;
+	if (t1->count < t2->count)
+		return -1;
+	if (t1->count == t2->count)
+		return 0;
+	return 1;
+}
+
+static bool
+is_leaf (octree_t *node)
+{
+	int i;
+	assert (node != NULL);
+	for (i = 0; i < 8; ++i) {
+		if (node->children [i] != NULL)
+			return false;
+	}
+	return true;
+}
+
+static octree_t*
+collapse_node (octree_t *node)
+{
+	int i;
+	assert (!is_leaf (node));
+	for (i = 0; i < 8; ++i) {
+		octree_t *c = node->children [i];
+		if (c == NULL)
+			continue;
+		assert (is_leaf (c));
+		node->red += c->red;
+		node->green += c->green;
+		node->blue += c->blue;
+		free (c);
+		node->children [i] = NULL;
+	}
+	node = node->parent;
+	if (node == NULL)
+		return NULL;
+	for (i = 0; i < 8; ++i) {
+		octree_t *c = node->children [i];
+		if (c != NULL && !is_leaf (c))
+			return NULL;
+	}
+	return node;
+}
+
+static int
+reduce_octree (octree_t *tree, int max, color_t *colors)
+{
+	int num_candidates = 0;
+	octree_t **candidates;
+	int first = 0;
+	int i;
+
+	gather_collapse_candidates (tree, &num_candidates, NULL);
+
+	candidates = malloc (sizeof (octree_t*) * num_candidates);
+	printf ("%u - %d\n", tree->count, num_candidates);
+	num_candidates = 0;
+	gather_collapse_candidates (tree, &num_candidates, candidates);
+
+	qsort (candidates, num_candidates, sizeof (octree_t*), compare_candidates);
+
+	while (num_candidates - first > max) {
+		octree_t *new_candidate = collapse_node (candidates [first]);
+		++first;
+		if (new_candidate != NULL) {
+			int i = --first;
+			while (i < num_candidates - 1 && new_candidate->count > candidates [i + 1]->count) {
+				candidates [i] = candidates [i + 1];
+				++i;
+			}
+			candidates [i] = new_candidate;
+		}
+	}
+
+	for (i = 0; first + i < num_candidates; ++i) {
+		octree_t *node = candidates [first + i];
+		collapse_node (node);
+		colors [i].red = node->red / node->count;
+		colors [i].green = node->green / node->count;
+		colors [i].blue = node->blue / node->count;
+		printf ("%3d  %3d  %3d   %d\n", colors [i].red, colors [i].green, colors [i].blue, node->count);
+	}
+
+	return i;
+}
+
+#define NUM_COLORS	16
+
 int
 main (void)
 {
 	int width, height;
 	unsigned char *data = read_image ("in.jpg", &width, &height);
 	octree_t *tree = build_octree (data, height, width * 3, width, 3, 6);
-	int num_candidates = 0;
-	octree_t **candidates;
-	gather_collapse_candidates (tree, &num_candidates, NULL);
-	candidates = malloc (sizeof (octree_t*) * num_candidates);
-	num_candidates = 0;
-	gather_collapse_candidates (tree, &num_candidates, candidates);
-	printf ("%u - %d\n", tree->count, num_candidates);
+	color_t colors [NUM_COLORS];
+	int num = reduce_octree (tree, 16, colors);
 	return 0;
 }
---
+++ palette.h
@@ -0,0 +1,10 @@
+#ifndef __SCHANI_PALETTE_H__
+#define __SCHANI_PALETTE_H__
+
+typedef struct {
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+} color_t;
+
+#endif
